5a96febb5fd82d0591d02a6cbb28ab5a37ac6d72
Sangwonlee93 <lsw0833@hanmail.net>
2018-05-24 20:10:40 +0900

    2013104098 0524

--- django-rest-tutorial/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sangwonlee\desktop\django-rest-tutorial\django-rest-tutorial\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
